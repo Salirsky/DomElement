69ceade18a7447b85ef015304ab9cb0ab5addbed
Marina Belova <83077065+Salirsky@users.noreply.github.com>
2022-05-17 23:33:35 +0300

    Trying different variants for elem.style

--- script.js
+++ script.js
@@ -8,11 +8,43 @@ let c = "100px";
 let d = "#2E16B1";
 let e = "28px";
 let f = " ";
-let newElem;
-let rectElem;
-let x;
-let y;
 let elem;
+let rectElem;
+let newElem;
+let x = 0;
+let y = 0;
+
+const foo = function (event) {
+  if (
+    event.code != "ArrowRight" &&
+    event.code != "ArrowLeft" &&
+    event.code != "ArrowUp" &&
+    event.code != "ArrowDown"
+  )
+    return;
+
+  if (event.code == "ArrowRight") x += 10;
+  if (event.code == "ArrowLeft") x -= 10;
+  if (event.code == "ArrowUp") y -= 10;
+  if (event.code == "ArrowDown") y += 10;
+
+  // elem.style.left = x + "px";
+  // elem.style.top = y + "px";
+
+  x = x + "px";
+  y = y + "px";
+
+  //Это не работает =/
+  elem.style.cssText = `left: ${x};
+  top: ${y};  `;
+
+  console.log(elem);
+  console.log(rectElem);
+  console.log(x);
+  console.log(y);
+
+  console.log(DomElement);
+};
 
 function DomElement(selector, height, width, bg, fontSize, text, x, y) {
   this.selector = selector;
@@ -32,22 +64,24 @@ function DomElement(selector, height, width, bg, fontSize, text, x, y) {
   }
   newElem.textContent = text;
 
-  newElem.style.cssText = `selector: ${this.selector}; 
-  height: ${this.height}; 
-  width: ${this.width}; 
-  background: ${this.bg}; 
-  fontSize: ${this.fontSize}; 
-  position: absolute; 
-  display: inline-block;
-  cursor: pointer;
-  margin: 0px;
-
-  `;
-  //   left = ${x} + "px";
-  //   top = ${y} + "px";
   rectElem = newElem.getBoundingClientRect();
   this.x = rectElem.x + 10;
   this.y = rectElem.y + 10;
+
+  newElem.style.cssText = `selector: ${this.selector}; 
+    height: ${this.height}; 
+    width: ${this.width}; 
+    background: ${this.bg}; 
+    fontSize: ${this.fontSize}; 
+    position: absolute; 
+    display: inline-block;
+    cursor: pointer;
+    margin: 0px;
+    left: ${this.x} + "px";
+    top: ${this.y} + "px";
+    `;
+  // left = ${x} + "px";
+  // top = ${y} + "px";
 
   console.log(rectElem);
   console.log(x);
@@ -58,45 +92,8 @@ function DomElement(selector, height, width, bg, fontSize, text, x, y) {
   document.addEventListener("keydown", foo);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM готов!");
-
-  const elem = new DomElement(a, b, c, d, e, f);
-  elem.tabIndex = 0;
-
-  console.log(elem);
-
-  //   document.addEventListener("keydown", foo);
-});
-
-const foo = function (event) {
-  if (
-    event.code != "ArrowRight" &&
-    event.code != "ArrowLeft" &&
-    event.code != "ArrowUp" &&
-    event.code != "ArrowDown"
-  )
-    return;
-
-  if (event.code == "ArrowRight") x += 10;
-  if (event.code == "ArrowLeft") x -= 10;
-  if (event.code == "ArrowUp") y -= 10;
-  if (event.code == "ArrowDown") y += 10;
-
-  //   keyControl(elem);
-
-  elem.style.left = x + "px";
-  elem.style.top = y + "px";
-
-  //   console.log(event);
-  console.log(elem);
-  console.log(rectElem);
-  console.log(x);
-  console.log(y);
-};
-
-// const keyControl = function (el) {
-//   el.style.left = x + "px";
-//   el.style.top = y + "px";
-// };
-
+elem = new DomElement(a, b, c, d, e, f);
+elem.tabIndex = 0;
+elem.style;
+console.log(elem.__proto__);
+console.log(elem.style);
