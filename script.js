854d9864fd272a2d6ef5497ada4b74f91be6b8b0
Marina Belova <83077065+Salirsky@users.noreply.github.com>
2022-05-17 22:44:43 +0300

    Add document.addEventListener("keydown", foo);

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ let c = "100px";
 let d = "#2E16B1";
 let e = "28px";
 let f = " ";
-let domElement;
+let newElem;
+let rectElem;
+let x;
+let y;
+let elem;
 
-function DomElement(selector, height, width, bg, fontSize, text) {
+function DomElement(selector, height, width, bg, fontSize, text, x, y) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
   this.textContent = text;
-  let newElem;
 
   if (selector.startsWith(".")) {
     newElem = document.createElement("div");
@@ -29,29 +32,44 @@ function DomElement(selector, height, width, bg, fontSize, text) {
   }
   newElem.textContent = text;
 
-  newElem.style.cssText = `selector: ${this.selector}; height: ${this.height}; width: ${this.width}; background: ${this.bg}; fontSize: ${this.fontSize}; position: absolute, display: inline-block;
+  newElem.style.cssText = `selector: ${this.selector}; 
+  height: ${this.height}; 
+  width: ${this.width}; 
+  background: ${this.bg}; 
+  fontSize: ${this.fontSize}; 
+  position: absolute; 
+  display: inline-block;
   cursor: pointer;
-  margin: 0;`;
+  margin: 0px;
+
+  `;
+  //   left = ${x} + "px";
+  //   top = ${y} + "px";
+  rectElem = newElem.getBoundingClientRect();
+  this.x = rectElem.x + 10;
+  this.y = rectElem.y + 10;
+
+  console.log(rectElem);
+  console.log(x);
+  console.log(y);
 
   body.insertBefore(newElem, null);
+
+  document.addEventListener("keydown", foo);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM готов!");
 
-  newEl();
+  const elem = new DomElement(a, b, c, d, e, f);
+  elem.tabIndex = 0;
 
-  //script.js:44 Uncaught TypeError: domElement.addEventListener is not a function
-  // at HTMLDocument.<anonymous> (script.js:44:14)
-  domElement.addEventListener("keydown", keyControl);
+  console.log(elem);
+
+  //   document.addEventListener("keydown", foo);
 });
 
-const newEl = function () {
-  domElement = new DomElement(a, b, c, d, e, f);
-  domElement.tabIndex = 0;
-};
-
-const keyControl = function (event) {
+const foo = function (event) {
   if (
     event.code != "ArrowRight" &&
     event.code != "ArrowLeft" &&
@@ -60,19 +78,25 @@ const keyControl = function (event) {
   )
     return;
 
-  let rectElem = this.getBoundingClientRect();
-  let x = rectElem.x + 10,
-    y = rectElem.y + 10;
-
   if (event.code == "ArrowRight") x += 10;
   if (event.code == "ArrowLeft") x -= 10;
   if (event.code == "ArrowUp") y -= 10;
   if (event.code == "ArrowDown") y += 10;
 
-  this.style.position = "absolute";
-  this.style.left = x + "px";
-  this.style.top = y + "px";
+  //   keyControl(elem);
+
+  elem.style.left = x + "px";
+  elem.style.top = y + "px";
+
+  //   console.log(event);
+  console.log(elem);
+  console.log(rectElem);
+  console.log(x);
+  console.log(y);
 };
 
-
+// const keyControl = function (el) {
+//   el.style.left = x + "px";
+//   el.style.top = y + "px";
+// };
 
